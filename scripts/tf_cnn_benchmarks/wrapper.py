1ec2115cab4176f558206df5b5a1c19955fe1d1f
Yuanhong Wang <wayuanho@aiinfra-win.corp.microsoft.com>
2018-09-04 05:57:39 +0000

    add wrapper to set CUDA_VISIBLE_DEVICES

--- scripts/tf_cnn_benchmarks/wrapper.py
+++ scripts/tf_cnn_benchmarks/wrapper.py
@@ -8,4 +8,6 @@ if_ps = (sys.argv[1] == 'ps')
 if if_ps:
     os.environ['CUDA_VISIBLE_DEVICES']=''
 
-os.system('%s %s/benchmark_cnn.py %s' % (sys.executable, CURRENT_PATH, ' '.join(sys.argv[2:])))
+cmd = '%s %s/tf_cnn_benchmarks.py %s' % (sys.executable, CURRENT_PATH, ' '.join(sys.argv[2:]))
+code = os.system(cmd)
+print('run %s and return %s' % (cmd, code))
